7c33d70dcc7a83566fe15b617fd1eaa476ee6907
王鹏 <wangpeng5@iie.ac.cn>
2020-04-20 14:53:14 +0800

    Add all CSRs of vector extension and testcases

--- llvm/test/MC/RISCV/user-csr-names.s
+++ llvm/test/MC/RISCV/user-csr-names.s
@@ -585,3 +585,88 @@ csrrs t2, 0xC1E, zero
 csrrs t1, hpmcounter31, zero
 # uimm12
 csrrs t2, 0xC1F, zero
+
+# vstart
+# name
+# CHECK-INST: csrrs t1, vstart, zero
+# CHECK-ENC:  encoding: [0x73,0x23,0x80,0x00]
+# CHECK-INST-ALIAS: csrr t1, vstart
+# uimm12
+# CHECK-INST: csrrs t2, vstart, zero
+# CHECK-ENC:  encoding: [0xf3,0x23,0x80,0x00]
+# CHECK-INST-ALIAS: csrr t2, vstart
+# name
+csrrs t1, vstart, zero
+# uimm12
+csrrs t2, 0x008, zero
+
+# vxsat
+# name
+# CHECK-INST: csrrs t1, vxsat, zero
+# CHECK-ENC:  encoding: [0x73,0x23,0x90,0x00]
+# CHECK-INST-ALIAS: csrr t1, vxsat
+# uimm12
+# CHECK-INST: csrrs t2, vxsat, zero
+# CHECK-ENC:  encoding: [0xf3,0x23,0x90,0x00]
+# CHECK-INST-ALIAS: csrr t2, vxsat
+# name
+csrrs t1, vxsat, zero
+# uimm12
+csrrs t2, 0x009, zero
+
+# vxrm
+# name
+# CHECK-INST: csrrs t1, vxrm, zero
+# CHECK-ENC:  encoding: [0x73,0x23,0xa0,0x00]
+# CHECK-INST-ALIAS: csrr t1, vxrm
+# uimm12
+# CHECK-INST: csrrs t2, vxrm, zero
+# CHECK-ENC:  encoding: [0xf3,0x23,0xa0,0x00]
+# CHECK-INST-ALIAS: csrr t2, vxrm
+# name
+csrrs t1, vxrm, zero
+# uimm12
+csrrs t2, 0x00A, zero
+
+# vl
+# name
+# CHECK-INST: csrrs t1, vl, zero
+# CHECK-ENC:  encoding: [0x73,0x23,0x00,0xc2]
+# CHECK-INST-ALIAS: csrr t1, vl
+# uimm12
+# CHECK-INST: csrrs t2, vl, zero
+# CHECK-ENC:  encoding: [0xf3,0x23,0x00,0xc2]
+# CHECK-INST-ALIAS: csrr t2, vl
+# name
+csrrs t1, vl, zero
+# uimm12
+csrrs t2, 0xC20, zero
+
+# vtype
+# name
+# CHECK-INST: csrrs t1, vtype, zero
+# CHECK-ENC:  encoding: [0x73,0x23,0x10,0xc2]
+# CHECK-INST-ALIAS: csrr t1, vtype
+# uimm12
+# CHECK-INST: csrrs t2, vtype, zero
+# CHECK-ENC:  encoding: [0xf3,0x23,0x10,0xc2]
+# CHECK-INST-ALIAS: csrr t2, vtype
+# name
+csrrs t1, vtype, zero
+# uimm12
+csrrs t2, 0xC21, zero
+
+# vlenb
+# name
+# CHECK-INST: csrrs t1, vlenb, zero
+# CHECK-ENC:  encoding: [0x73,0x23,0x20,0xc2]
+# CHECK-INST-ALIAS: csrr t1, vlenb
+# uimm12
+# CHECK-INST: csrrs t2, vlenb, zero
+# CHECK-ENC:  encoding: [0xf3,0x23,0x20,0xc2]
+# CHECK-INST-ALIAS: csrr t2, vlenb
+# name
+csrrs t1, vlenb, zero
+# uimm12
+csrrs t2, 0xC22, zero
+
