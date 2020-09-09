ac15998d73a6a565433dfd6a90fbfd7cf2f8644d
liaochunyu <chunyu@iscas.ac.cn>
2020-09-09 15:05:59 +0800

    [clang] add vfmv_v_f

---
+++ clang/test/CodeGen/riscv_intrinsics/riscv_vfmv_v_f.c
@@ -0,0 +1,11 @@
+// RUN: %clang_cc1 -triple riscv64-unknown-linux-elf -S -emit-llvm  %s -o - |  FileCheck %s
+#include <stdbool.h>
+#include <stddef.h>
+#include <riscv_vector.h>
+
+vfloat32m1_t  __attribute__((noinline)) testmax8(float src) {
+  return vfmv_v_f_f32m1(src);
+}
+
+
+//CHECK: %{{.*}} = call <vscale x 2 x float> @llvm.riscv.vfmv.v.f.nxv2f32.f32(float %{{.*}})
