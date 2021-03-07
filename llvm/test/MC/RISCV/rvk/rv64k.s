b71fb7a2dc843fe70d2af54e44a61c5e566999b2
wuxinlong <821408745@qq.com>
2021-03-07 14:40:52 +0800

    fix: error trailing whitespace when building

--- llvm/test/MC/RISCV/rvk/rv64k.s
+++ llvm/test/MC/RISCV/rvk/rv64k.s
@@ -1,13 +1,10 @@
 # RUN: llvm-mc -triple=riscv64 -show-encoding --mattr=+experimental-k %s \
 # RUN:      | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
-
 # RUN: not llvm-mc -triple=riscv64 -show-encoding %s 2>&1 \
 # RUN:      | FileCheck %s --check-prefix=CHECK-ERROR
-
 # RUN: llvm-mc -triple=riscv64 -filetype=obj --mattr=+experimental-k %s \
 # RUN:      | llvm-objdump -d --mattr=+experimental-k - \
 # RUN:      | FileCheck %s --check-prefix=CHECK-INST
-
 # RUN: llvm-mc -triple=riscv64 -filetype=obj --mattr=+experimental-k %s \
 # RUN:      | llvm-objdump -d - | FileCheck %s --check-prefix=CHECK-UNKNOWN
 
