cb92fdc72343720caaaba8eabe7ba3f6137ed182
qixingxue <qixingxue@outlook.com>
2021-03-08 16:49:40 +0800

    Split test cases

--- llvm/test/MC/RISCV/rvk/rv32k-only-valid.s
+++ llvm/test/MC/RISCV/rvk/rv32k-only-valid.s
@@ -8,56 +8,6 @@
 # RUN: llvm-mc -triple=riscv32 -filetype=obj --mattr=+experimental-k %s \
 # RUN:      | llvm-objdump -d - | FileCheck %s --check-prefix=CHECK-UNKNOWN
 
-# CHECK-INST: sha256sum0 a0, a1
-# CHECK-ENCODING: [0x13,0x95,0x05,0x10]
-# CHECK-ERROR: instruction requires the following: 'K' (Scalar Cryptography Instructions)
-# CHECK-UNKNOWN: 13 95 05 10 <unknown>
-sha256sum0 a0, a1
-
-# CHECK-INST: sha256sum1 a0, a1
-# CHECK-ENCODING: [0x13,0x95,0x15,0x10]
-# CHECK-ERROR: instruction requires the following: 'K' (Scalar Cryptography Instructions)
-# CHECK-UNKNOWN: 13 95 15 10 <unknown>
-sha256sum1 a0, a1
-
-# CHECK-INST: sha256sig0 a0, a1
-# CHECK-ENCODING: [0x13,0x95,0x25,0x10]
-# CHECK-ERROR: instruction requires the following: 'K' (Scalar Cryptography Instructions)
-# CHECK-UNKNOWN: 13 95 25 10 <unknown>
-sha256sig0 a0, a1
-
-# CHECK-INST: sha256sig1 a0, a1
-# CHECK-ENCODING: [0x13,0x95,0x35,0x10]
-# CHECK-ERROR: instruction requires the following: 'K' (Scalar Cryptography Instructions)
-# CHECK-UNKNOWN: 13 95 35 10 <unknown>
-sha256sig1 a0, a1
-
-# CHECK-INST: sm3p0 a0, a1
-# CHECK-ENCODING: [0x13,0x95,0x85,0x10]
-# CHECK-ERROR: instruction requires the following: 'K' (Scalar Cryptography Instructions)
-# CHECK-UNKNOWN: 13 95 85 10 <unknown>
-sm3p0 a0, a1
-
-# CHECK-INST: sm3p1 a0, a1
-# CHECK-ENCODING: [0x13,0x95,0x95,0x10]
-# CHECK-ERROR: instruction requires the following: 'K' (Scalar Cryptography Instructions)
-# CHECK-UNKNOWN: 13 95 95 10 <unknown>
-sm3p1 a0, a1
-
-# CHECK-INST: csrr a0, mnoise 
-# CHECK-ENCODING: [0x73,0x25,0x90,0x7a]
-# CHECK-ERROR: instruction requires the following: 'K' (Scalar Cryptography Instructions)
-# CHECK-UNKNOWN: 73 25 90 7a
-getnoise a0
-
-# CHECK-INST: csrr a0, mentropy
-# CHECK-ENCODING: [0x73,0x25,0x50,0xf1]
-# CHECK-ERROR: instruction requires the following: 'K' (Scalar Cryptography Instructions)
-# CHECK-UNKNOWN: 73 25 50 f1
-pollentropy a0
-
-# RV32 only
-
 # CHECK-INST: sha512sum0r a0, a1, a2
 # CHECK-ENCODING: [0x33,0x85,0xc5,0x50]
 # CHECK-ERROR: instruction requires the following: 'K' (Scalar Cryptography Instructions)
