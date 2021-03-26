22ade73a26a28c562498d54d050257dd58ee9243
wuxinlong <821408745@qq.com>
2021-03-26 13:37:50 +0800

    - test: add `zkb`&`zkg` test  case - style: order the variable and functions in Alphabetical order

---
+++ llvm/test/MC/RISCV/rvk/rv32zkg-valid.s
@@ -0,0 +1,35 @@
+# With K extension:
+# RUN: llvm-mc -triple=riscv32 -show-encoding --mattr=+experimental-k %s \
+# RUN:      | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
+# RUN: llvm-mc -triple=riscv32 -filetype=obj --mattr=+experimental-k %s \
+# RUN:      | llvm-objdump -d --mattr=+experimental-k - \
+# RUN:      | FileCheck %s --check-prefix=CHECK-INST
+
+# With Zkn extension:
+# RUN: llvm-mc -triple=riscv32 -show-encoding --mattr=+experimental-zkn %s \
+# RUN:      | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
+# RUN: llvm-mc -triple=riscv32 -filetype=obj --mattr=+experimental-zkn %s \
+# RUN:      | llvm-objdump -d --mattr=+experimental-zkn - \
+# RUN:      | FileCheck %s --check-prefix=CHECK-INST
+
+# With Zks extension:
+# RUN: llvm-mc -triple=riscv32 -show-encoding --mattr=+experimental-zks %s \
+# RUN:      | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
+# RUN: llvm-mc -triple=riscv32 -filetype=obj --mattr=+experimental-zks %s \
+# RUN:      | llvm-objdump -d --mattr=+experimental-zks - \
+# RUN:      | FileCheck %s --check-prefix=CHECK-INST
+
+# With Zkg extension:
+# RUN: llvm-mc -triple=riscv32 -show-encoding --mattr=+experimental-zkg %s \
+# RUN:      | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
+# RUN: llvm-mc -triple=riscv32 -filetype=obj --mattr=+experimental-zkg %s \
+# RUN:      | llvm-objdump -d --mattr=+experimental-zkg - \
+# RUN:      | FileCheck %s --check-prefix=CHECK-INST
+
+# CHECK-INST: clmul t0, t1, t2
+# CHECK-ENCODING: [0xb3,0x12,0x73,0x0a]
+clmul t0, t1, t2
+
+# CHECK-INST: clmulh t0, t1, t2
+# CHECK-ENCODING: [0xb3,0x32,0x73,0x0a]
+clmulh t0, t1, t2
