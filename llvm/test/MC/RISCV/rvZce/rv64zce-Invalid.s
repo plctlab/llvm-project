cc529d6f4d32328703eb0630aa9d1014a37cf299
wuxinlong <821408745@qq.com>
2021-06-08 22:22:05 +0800

    complete testcase

---
+++ llvm/test/MC/RISCV/rvZce/rv64zce-Invalid.s
@@ -0,0 +1,29 @@
+# RUN: not llvm-mc -triple=riscv64 -mattr=experimental-zce -mattr=m -riscv-no-aliases -show-encoding < %s 2>&1 \
+# RUN:     | FileCheck -check-prefixes=CHECK-ERROR %s
+
+# CHECK-ERROR: error: immediate must be an integer in the range [0, 6]
+tbljalm 7
+
+# CHECK-ERROR: error: immediate must be an integer in the range [0, 55]
+tblj 56
+
+# CHECK-ERROR: error: immediate must be an integer in the range [0, 191]
+tbljal 192
+
+# CHECK-ERROR: error: immediate must be an integer in the range [0, 15]
+c.lbu a5, 16(a4)
+
+# CHECK-ERROR: error: immediate must be a multiple of 2 bytes in the range [0, 30]
+c.lhu a5, 31(a4)
+
+# CHECK-ERROR: error: immediate must be an integer in the range [0, 15]
+c.lb a5, 16(a4)
+
+# CHECK-ERROR: error: immediate must be a multiple of 2 bytes in the range [0, 30]
+c.lh a5, 31(a4)
+
+# CHECK-ERROR: error: immediate must be an integer in the range [0, 15]
+c.sb a5, 16(a4)
+
+# CHECK-ERROR: error: immediate must be a multiple of 2 bytes in the range [0, 30]
+c.sh a5, 31(a4)
