1ec2ae8fd197e9efa69ac11084a770eb870101b3
QuarticCat <QuarticCat@protonmail.com>
2021-11-21 17:49:59 +0800

    Add simd config

---
+++ libcxx/include/__simd/abi/all.h
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//===-------------------------- __simd/abi/all.h --------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#ifndef _LIBCPP___SIMD_ABI_ALL_H
+#define _LIBCPP___SIMD_ABI_ALL_H
+
+#include <__simd/abi/scalar.h>
+#include <__simd/abi/array.h>
+#include <__simd/abi/vec_ext.h>
+
+#endif // _LIBCPP___SIMD_ABI_ALL_H
