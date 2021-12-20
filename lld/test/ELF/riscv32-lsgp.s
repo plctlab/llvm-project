07940dd3e44be8cc6fadf7f0a9428a4f82ccfd1a
Wu Xinlong <821408745@qq.com>
2021-12-20 12:14:01 +0800

    fix testcase

--- lld/test/ELF/riscv32-lsgp.s
+++ lld/test/ELF/riscv32-lsgp.s
@@ -7,54 +7,60 @@
 # RUN: ld.lld --undefined=__global_pointer$ %t.rv32relax.o %t.lds -o %t.rv32relax -mzce-lsgp
 # RUN: ld.lld --undefined=__global_pointer$ %t.rv32.o %t.lds -o %t.rv32 -mzce-lsgp --no-relax
 
-# RUN: llvm-objdump  --triple=riscv32 --mattr=+c,+m,+a,+experimental-zce -d -M no-aliases --no-show-raw-insn  %t.rv32relax | FileCheck --check-prefix=LSGP-RELAX %s
 # RUN: llvm-objdump  --triple=riscv32 --mattr=+c,+m,+a,+experimental-zce -d -M no-aliases --no-show-raw-insn  %t.rv32 | FileCheck --check-prefix=LSGP %s
+# RUN: llvm-objdump  --triple=riscv32 --mattr=+c,+m,+a,+experimental-zce -d -M no-aliases --no-show-raw-insn  %t.rv32relax | FileCheck --check-prefix=LSGP-RELAX %s
 
-
-# LSGP-RELAX:       addi  a0, gp, -1792
-# LSGP-RELAX-NEXT:  lwgp    a0, -1792(gp)
-# LSGP-RELAX-NEXT:  swgp    a0, -1792(gp)
-
-# LSGP-RELAX:       addi    a0, a0, 1280
-# LSGP-RELAX-NEXT:  lwgp    a0, 23808(gp)
-# LSGP-RELAX-NEXT:  swgp    a0, 23808(gp)
-
-# LSGP-RELAX-NEXT:  lwgp    a0, -1792(gp)
-# LSGP-RELAX-NEXT:  lwgp    a0, 23808(gp)
-# LSGP-RELAX-NEXT:  swgp    a0, -1792(gp)
-# LSGP-RELAX-NEXT:  swgp    a0, 23808(gp)
-
-# LSGP-RELAX-NEXT:  lw a0, 256(a0)
-
-# LSGP:       addi  a0, a0, 256
+# LSGP:       lui   a0, 512
+# LSGP-NEXT:  addi  a1, a0, 256
 # LSGP-NEXT:  lw    a0, 256(a0)
 # LSGP-NEXT:  sw    a0, 256(a0)
 
-# LSGP:       addi  a0, a0, 1280
+# LSGP:       lui   a0, 518
+# LSGP-NEXT:  addi  a1, a0, 1280
 # LSGP-NEXT:  lw    a0, 1280(a0)
 # LSGP-NEXT:  sw    a0, 1280(a0)
 
+# LSGP-NEXT:  lui   a0, 512
+# LSGP-NEXT:  addi  a1, a0, 256
 # LSGP-NEXT:  lwgp  a0, -1792(gp)
-# LSGP-NEXT:  lwgp  a0, 23808(gp)
 # LSGP-NEXT:  swgp  a0, -1792(gp)
+# LSGP-NEXT:  lui   a0, 518
+# LSGP-NEXT:  addi  a1, a0, 1280
+# LSGP-NEXT:  lwgp  a0, 23808(gp)
 # LSGP-NEXT:  swgp  a0, 23808(gp)
 
-# LSGP-NEXT:  lw a0, 256(a0)
+# LSGP-RELAX:  addi    a1, gp, -1792
+# LSGP-RELAX-NEXT:       lwgp    a0, -1792(gp)
+# LSGP-RELAX-NEXT:  swgp    a0, -1792(gp)
+
+# LSGP-RELAX-NEXT:  addi    a1, gp, -768
+# LSGP-RELAX:       lwgp    a0, 23808(gp)
+# LSGP-RELAX-NEXT:  swgp    a0, 23808(gp)
+
+# LSGP-RELAX-NEXT:  addi    a1, gp, -1792
+# LSGP-RELAX-NEXT:  lwgp    a0, -1792(gp)
+# LSGP-RELAX-NEXT:  swgp    a0, -1792(gp)
+# LSGP-RELAX-NEXT:  addi    a1, gp, -768
+# LSGP-RELAX-NEXT:  lwgp    a0, 23808(gp)
+# LSGP-RELAX-NEXT:  swgp    a0, 23808(gp)
 
 .global _start
 _start:
-  addi a0, a0, %lo(gI)
+  lui a0, %hi(gI)
+  addi a1, a0, %lo(gI)
   lw a0, %lo(gI)(a0)
   sw a0, %lo(gI)(a0)
   
-  addi a0, a0, %lo(gStart)
+  lui a0, %hi(gStart)
+  addi a1, a0, %lo(gStart)
   lw a0, %lo(gStart)(a0)
   sw a0, %lo(gStart)(a0)
 
+  lui a0, %hi(gI)
+  addi a1, a0, %lo(gI)
   lwgp a0, %lo(gI)(gp)
-  lwgp a0, %lo(gStart)(gp)
   swgp a0, %lo(gI)(gp)
+  lui a0, %hi(gStart)
+  addi a1, a0, %lo(gStart)
+  lwgp a0, %lo(gStart)(gp)
   swgp a0, %lo(gStart)(gp)
-
-  lw a0, 256(a0)
-  sw a0, 256(a0)
--- lld/test/ELF/riscv64-lsgp.s
+++ lld/test/ELF/riscv64-lsgp.s
@@ -7,81 +7,101 @@
 # RUN: ld.lld --undefined=__global_pointer$ %t.rv64relax.o %t.lds -o %t.rv64relax -mzce-lsgp
 # RUN: ld.lld --undefined=__global_pointer$ %t.rv64.o %t.lds -o %t.rv64 -mzce-lsgp --no-relax
 
-# RUN: llvm-objdump  --triple=riscv64 --mattr=+c,+m,+a,+experimental-zce -d -M no-aliases --no-show-raw-insn %t.rv64relax | FileCheck --check-prefix=LSGP-RELAX %s
 # RUN: llvm-objdump  --triple=riscv64 --mattr=+c,+m,+a,+experimental-zce -d -M no-aliases --no-show-raw-insn %t.rv64 | FileCheck --check-prefix=LSGP %s
+# RUN: llvm-objdump  --triple=riscv64 --mattr=+c,+m,+a,+experimental-zce -d -M no-aliases --no-show-raw-insn %t.rv64relax | FileCheck --check-prefix=LSGP-RELAX %s
 
 
-# LSGP-RELAX:       addi  a0, gp, -1792
-# LSGP-RELAX-NEXT:  lwgp    a0, -1792(gp)
-# LSGP-RELAX-NEXT:  swgp    a0, -1792(gp)
-# LSGP-RELAX-NEXT:  ldgp    a0, -1792(gp)
-# LSGP-RELAX-NEXT:  sdgp    a0, -1792(gp)
-
-# LSGP-RELAX:       addi    a0, a0, 1280
-# LSGP-RELAX-NEXT:  lwgp    a0, 23808(gp)
-# LSGP-RELAX-NEXT:  swgp    a0, 23808(gp)
-# LSGP-RELAX-NEXT:  ldgp    a0, 23808(gp)
-# LSGP-RELAX-NEXT:  sdgp    a0, 23808(gp)
-
-# LSGP-RELAX-NEXT:  lwgp    a0, -1792(gp)
-# LSGP-RELAX-NEXT:  lwgp    a0, 23808(gp)
-# LSGP-RELAX-NEXT:  swgp    a0, -1792(gp)
-# LSGP-RELAX-NEXT:  swgp    a0, 23808(gp)
-
-# LSGP-RELAX-NEXT:  ldgp    a0, -1792(gp)
-# LSGP-RELAX-NEXT:  ldgp    a0, 23808(gp)
-# LSGP-RELAX-NEXT:  sdgp    a0, -1792(gp)
-# LSGP-RELAX-NEXT:  sdgp    a0, 23808(gp)
-
-# LSGP-RELAX-NEXT:  lw a0, 256(a0)
-
-# LSGP:       addi  a0, a0, 256
+# LSGP:       lui     a0, 512
+# LSGP-NEXT:  addi    a1, a0, 256
 # LSGP-NEXT:  lw    a0, 256(a0)
 # LSGP-NEXT:  sw    a0, 256(a0)
 # LSGP-NEXT:  ld    a0, 256(a0)
 # LSGP-NEXT:  sd    a0, 256(a0)
 
-# LSGP:       addi  a0, a0, 1280
+# LSGP:       lui   a0, 518
+# LSGP-NEXT:  addi    a1, a0, 1280
 # LSGP-NEXT:  lw    a0, 1280(a0)
 # LSGP-NEXT:  sw    a0, 1280(a0)
 # LSGP-NEXT:  ld    a0, 1280(a0)
 # LSGP-NEXT:  sd    a0, 1280(a0)
 
+# LSGP-NEXT:  lui   a0, 512
+# LSGP-NEXT:  addi    a1, a0, 256
 # LSGP-NEXT:  lwgp  a0, -1792(gp)
-# LSGP-NEXT:  lwgp  a0, 23808(gp)
 # LSGP-NEXT:  swgp  a0, -1792(gp)
+
+# LSGP:       lui   a0, 518
+# LSGP-NEXT:  addi    a1, a0, 1280
+# LSGP-NEXT:  lwgp  a0, 23808(gp)
 # LSGP-NEXT:  swgp  a0, 23808(gp)
 
+# LSGP-NEXT:  lui   a0, 512
+# LSGP-NEXT:  addi    a1, a0, 256
 # LSGP-NEXT:  ldgp  a0, -1792(gp)
-# LSGP-NEXT:  ldgp  a0, 23808(gp)
 # LSGP-NEXT:  sdgp  a0, -1792(gp)
+# LSGP:       lui   a0, 518
+# LSGP-NEXT:  addi    a1, a0, 1280
+# LSGP-NEXT:  ldgp  a0, 23808(gp)
 # LSGP-NEXT:  sdgp  a0, 23808(gp)
 
-# LSGP-NEXT:  lw a0, 256(a0)
+# LSGP-RELAX:       addi    a1, gp, -1792
+# LSGP-RELAX-NEXT:  lwgp    a0, -1792(gp)
+# LSGP-RELAX-NEXT:  swgp    a0, -1792(gp)
+# LSGP-RELAX-NEXT:  ldgp    a0, -1792(gp)
+# LSGP-RELAX-NEXT:  sdgp    a0, -1792(gp)
+
+# LSGP-RELAX:       addi    a1, gp, -768
+# LSGP-RELAX-NEXT:  lwgp    a0, 23808(gp)
+# LSGP-RELAX-NEXT:  swgp    a0, 23808(gp)
+# LSGP-RELAX-NEXT:  ldgp    a0, 23808(gp)
+# LSGP-RELAX-NEXT:  sdgp    a0, 23808(gp)
+
+# LSGP-RELAX-NEXT:  addi    a1, gp, -1792
+# LSGP-RELAX-NEXT:  lwgp    a0, -1792(gp)
+# LSGP-RELAX-NEXT:  swgp    a0, -1792(gp)
+# LSGP-RELAX-NEXT:  addi    a1, gp, -768
+# LSGP-RELAX-NEXT:  lwgp    a0, 23808(gp)
+# LSGP-RELAX-NEXT:  swgp    a0, 23808(gp)
+
+# LSGP-RELAX-NEXT:  addi    a1, gp, -1792
+# LSGP-RELAX-NEXT:  ldgp    a0, -1792(gp)
+# LSGP-RELAX-NEXT:  sdgp    a0, -1792(gp)
+# LSGP-RELAX-NEXT:  addi    a1, gp, -768
+# LSGP-RELAX-NEXT:  ldgp    a0, 23808(gp)
+# LSGP-RELAX-NEXT:  sdgp    a0, 23808(gp)
+
 
 .global _start
 _start:
-  addi a0, a0, %lo(gI)
+  lui a0, %hi(gI)
+  addi a1, a0, %lo(gI)
   lw a0, %lo(gI)(a0)
   sw a0, %lo(gI)(a0)
   ld a0, %lo(gI)(a0)
   sd a0, %lo(gI)(a0)
   
-  addi a0, a0, %lo(gStart)
+  lui a0, %hi(gStart)
+  addi a1, a0, %lo(gStart)
   lw a0, %lo(gStart)(a0)
   sw a0, %lo(gStart)(a0)
   ld a0, %lo(gStart)(a0)
   sd a0, %lo(gStart)(a0)
 
+  lui a0, %hi(gI)
+  addi a1, a0, %lo(gI)
   lwgp a0, %lo(gI)(gp)
-  lwgp a0, %lo(gStart)(gp)
   swgp a0, %lo(gI)(gp)
+  lui a0, %hi(gStart)
+  addi a1, a0, %lo(gStart)
+  lwgp a0, %lo(gStart)(gp)
   swgp a0, %lo(gStart)(gp)
 
+  lui a0, %hi(gI)
+  addi a1, a0, %lo(gI)
   ldgp a0, %lo(gI)(gp)
-  ldgp a0, %lo(gStart)(gp)
   sdgp a0, %lo(gI)(gp)
-  sdgp a0, %lo(gStart)(gp)
 
-  lw a0, 256(a0)
-  sw a0, 256(a0)
+  lui a0, %hi(gStart)
+  addi a1, a0, %lo(gStart)
+  ldgp a0, %lo(gStart)(gp)
+  sdgp a0, %lo(gStart)(gp)
