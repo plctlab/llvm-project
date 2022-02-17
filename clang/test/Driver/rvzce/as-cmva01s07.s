ec6095e7458ea2aab7b9dd300a479c5e9652a3d8
SForekeeper <zkliu6@gmail.com>
2022-02-17 20:26:16 +0800

    Remove clang cmva01s07 test case.

--- clang/test/Driver/rvzce/as-cmva01s07.s
+++
@@ -1,5 +0,0 @@
-// RUN: %clang --target=riscv32-unknown-elf -mzce-cmva01s07 -c %s -o %t
-// RUN: llvm-objdump -d --mattr=+experimental-zce %t | FileCheck %s
-
-c.mva01s07 s0, s1
-// CHECK: c.mva01s07 s0, s1
