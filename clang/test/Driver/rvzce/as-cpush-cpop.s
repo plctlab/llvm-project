6d5d330c0eb0841797ce0c1fda10a5385e355a98
SForeKeeper <zkliu6@gmail.com>
2022-02-25 13:26:50 +0800

    Fix objdump mattr in as-cpush-cpop due to confilct encoding.

--- clang/test/Driver/rvzce/as-cpush-cpop.s
+++ clang/test/Driver/rvzce/as-cpush-cpop.s
@@ -1,5 +1,5 @@
 // RUN: %clang --target=riscv32-unknown-elf -mzce-cpush-cpop -c %s -o %t
-// RUN: llvm-objdump -d --mattr=+experimental-zce -M no-aliases %t | FileCheck %s
+// RUN: llvm-objdump -d --mattr=zce-cpush-cpop -M no-aliases %t | FileCheck %s
 
 cm.popret {ra}, 16
 // CHECK: cm.popret {ra}, 16
