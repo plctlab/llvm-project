d0cfde1d4e4a0127a1d471fc4c59c99e521d1a7e
zhangyi <2289766608@qq.com>
2022-03-03 21:48:34 +0800

    Add test cases for simd.algorithms

---
+++ libcxx/test/std/experimental/simd/simd.elementwise/min.pass.cpp
@@ -0,0 +1,44 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+
+// <experimental/simd>
+//
+// template <class T, class Abi>
+// simd<T, Abi>
+// min(const simd<T, Abi>&, const simd<T, Abi>&) noexcept;
+
+#include <cassert>
+#include <cstdint>
+#include <algorithm>
+#include <experimental/simd>
+
+namespace ex = std::experimental::parallelism_v2;
+
+
+template <typename T, typename Abi>
+bool equal(ex::simd<T, Abi> a, ex::simd<T, Abi> b){
+  for(size_t i = 0; i < a.size(); i++){
+    if(a[i] != b[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+void test_min() {
+  int n = (int)ex::native_simd<int>::size();
+  assert(
+      equal(min(ex::native_simd<int>([](int i) { return i; }),
+                ex::native_simd<int>([n](int i) { return n - 1 - i; })),
+            ex::native_simd<int>([n](int i) { return std::min(i, n - 1 - i); })));
+}
+
+int main() { test_min(); }
