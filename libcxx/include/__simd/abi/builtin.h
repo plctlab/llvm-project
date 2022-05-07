cf8ee0f8e5f08cc8b92074f4eddbebc4668a16df
zhangyi <2289766608@qq.com>
2022-05-07 20:43:19 +0800

    fix some bugs of simd.operators and simd.whereexp

--- libcxx/include/__simd/abi/builtin.h
+++ libcxx/include/__simd/abi/builtin.h
@@ -90,25 +90,52 @@ struct __simd_traits {
 
   static _Simd __shift_right(_Simd __lhs, int __rhs) noexcept { return {__lhs.__data >> __rhs}; }
 
-  static _Simd __equal_to(_Simd __lhs, _Simd __rhs) noexcept { return {__lhs.__data == __rhs.__data}; }
+  static _Mask __equal_to(_Simd __lhs, _Simd __rhs) noexcept {
+     _Mask __mask;
+    for (size_t __i = 0; __i < _Abi::__simd_size; __i++) {
+      __mask.__data[__i] = __lhs.__data[__i] == __rhs.__data[__i];
+    }
+    return __mask;
+   }
 
-  static _Simd __not_equal_to(_Simd __lhs, _Simd __rhs) noexcept { return {__lhs.__data != __rhs.__data}; }
+  static _Mask __not_equal_to(_Simd __lhs, _Simd __rhs) noexcept {
+     _Mask __mask;
+    for (size_t __i = 0; __i < _Abi::__simd_size; __i++) {
+      __mask.__data[__i] = __lhs.__data[__i] != __rhs.__data[__i];
+    }
+    return __mask;
 
-  static _Simd __less_equal(_Simd __lhs, _Simd __rhs) noexcept { return {__lhs.__data <= __rhs.__data}; }
+   }
 
-  static _Simd __less(_Simd __lhs, _Simd __rhs) noexcept { return {__lhs.__data < __rhs.__data}; }
+  static _Mask __less_equal(_Simd __lhs, _Simd __rhs) noexcept {
+     _Mask __mask;
+    for (size_t __i = 0; __i < _Abi::__simd_size; __i++) {
+      __mask.__data[__i] = __lhs.__data[__i] <= __rhs.__data[__i];
+    }
+    return __mask;
 
-  static _Tp __hmin(_Simd __s) {
+   }
+
+  static _Mask __less(_Simd __lhs, _Simd __rhs) noexcept{
+     _Mask __mask;
+    for (size_t __i = 0; __i < _Abi::__simd_size; __i++) {
+      __mask.__data[__i] = __lhs.__data[__i] < __rhs.__data[__i];
+    }
+    return __mask;
+
+   }
+
+  static _Tp __hmin(_Simd __s) noexcept{
     _Tp __min = __s.__data[0];
-    for (auto __v : __s.__data)
-      __min = std::min(__min, __v);
+    for (size_t __i = 1; __i < _Abi::__simd_size; ++__i)
+      __min = std::min(__min, __s.__data[__i]);
     return __min;
   }
 
-  static _Tp __hmax(_Simd __s) {
+  static _Tp __hmax(_Simd __s) noexcept{
     _Tp __max = __s.__data[0];
-    for (auto __v : __s.__data)
-      __max = std::max(__max, __v);
+    for (size_t __i = 1; __i < _Abi::__simd_size; ++__i)
+      __max = std::max(__max, __s.__data[__i]);
     return __max;
   }
 
@@ -154,13 +181,12 @@ struct __simd_traits {
       __s.__data[__i] = __m.__data[__i] ? __v : __s.__data[__i];
     return __s;
   }
-#define _LIBCXX_MASKED_OP_(__op, __name)				\
-  static _Simd __masked##__name(_Simd&__s, _Mask __m, _Tp __v) noexcept \
-  {									\
-     for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)		\
+#define _LIBCXX_MASKED_OP_(__op, __name)                                \
+  static void __masked##__name(_Simd&__s, _Mask __m, _Tp __v) noexcept  \
+  {                                                                     \
+     for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)               \
        __s.__data[__i] = __m.__data[__i] ? __s.__data[__i] __op __v :   \
-	  __s.__data[__i];						\
-     return __s;							\
+    __s.__data[__i];                                                    \
   }
   _LIBCXX_MASKED_OP_(+, _plus)
   _LIBCXX_MASKED_OP_(-, _minus)
@@ -173,27 +199,24 @@ struct __simd_traits {
   _LIBCXX_MASKED_OP_(<<, _shift_left)
   _LIBCXX_MASKED_OP_(>>, _shift_right)
 #undef _LIBCXX_MASKED_OP_
-  static _Simd __masked_incre(_Simd& __s, _Mask __m) noexcept {
+  static void __masked_incre(_Simd& __s, _Mask __m) noexcept {
     for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
       __s.__data[__i] = __m.__data[__i] ? __s.__data[__i]++ : __s.__data[__i];
-    return __s;
   }
-  static _Simd __masked_decre(_Simd& __s, _Mask __m) noexcept {
+
+  static void __masked_decre(_Simd& __s, _Mask __m) noexcept {
     for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
       __s.__data[__i] = __m.__data[__i] ? __s.__data[__i]-- : __s.__data[__i];
-    return __s;
   }
   template<class _Up>
-  static _Simd __masked_load(_Simd& __s, _Mask __m, _Up* __mem) noexcept {
+  static void __masked_load(_Simd& __s, _Mask __m, _Up* __mem) noexcept {
     for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
-      __s.__data[__i] = __m.__data[__i] ? __mem[__i] : __s.__data[__i];
-    return __s;
+      __s.__data[__i] = __m.__data[__i] ? static_cast<_Tp>(__mem[__i]) : __s.__data[__i];
   }
  template<class _Up>
- static _Simd __masked_store(_Simd& __s, _Mask __m, _Up* __mem) noexcept {
+ static void __masked_store(const _Simd& __s,  _Mask __m, _Up* __mem) noexcept {
     for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
-      __mem[__i] = __m.__data[__i] ? __s.__data[__i] : __mem[__i];
-    return __s;
+      __mem[__i] = __m.__data[__i] ? static_cast<_Up>(__s.__data[__i]) : __mem[__i];
   }
 };
 
@@ -229,58 +252,59 @@ struct __mask_traits {
     for (size_t __i = 0; __i < _Abi::__simd_size; __i++)
       __mem[__i] = static_cast<_Up>(__s.__data[__i]);
   }
+  static _Mask __negate(_Mask __s) noexcept { return {{~__s.__data}}; }
 
-  static _Mask __logical_and(_Mask __lhs, _Mask __rhs) noexcept { return {__lhs.__data & __rhs.__data}; }
+  static _Mask __logical_and(_Mask __lhs, _Mask __rhs) noexcept { return {{__lhs.__data & __rhs.__data}}; }
 
-  static _Mask __logical_or(_Mask __lhs, _Mask __rhs) noexcept { return {__lhs.__data | __rhs.__data}; }
+  static _Mask __logical_or(_Mask __lhs, _Mask __rhs) noexcept { return {{__lhs.__data | __rhs.__data}}; }
 
-  static _Mask __bitwise_and(_Mask __lhs, _Mask __rhs) noexcept { return {__lhs.__data & __rhs.__data}; }
+  static _Mask __bitwise_and(_Mask __lhs, _Mask __rhs) noexcept { return {{__lhs.__data & __rhs.__data}}; }
 
-  static _Mask __bitwise_or(_Mask __lhs, _Mask __rhs) noexcept { return {__lhs.__data | __rhs.__data}; }
+  static _Mask __bitwise_or(_Mask __lhs, _Mask __rhs) noexcept { return {{__lhs.__data | __rhs.__data}}; }
 
-  static _Mask __bitwise_xor(_Mask __lhs, _Mask __rhs) noexcept { return {__lhs.__data ^ __rhs.__data}; }
+  static _Mask __bitwise_xor(_Mask __lhs, _Mask __rhs) noexcept { return {{__lhs.__data ^ __rhs.__data}}; }
 
-  static _Mask __equal_to(_Mask __lhs, _Mask __rhs) noexcept { return {__lhs.__data == __rhs.__data}; }
+  static _Mask __equal_to(_Mask __lhs, _Mask __rhs) noexcept { return {{__lhs.__data == __rhs.__data}}; }
 
-  static _Mask __not_equal_to(_Mask __lhs, _Mask __rhs) noexcept { return {__lhs.__data != __rhs.__data}; }
+  static _Mask __not_equal_to(_Mask __lhs, _Mask __rhs) noexcept { return {{__lhs.__data != __rhs.__data}}; }
 
   static bool __all_of(_Mask __s) noexcept {
-    for (auto __v : __s.__data)
-      if (!__v)
+    for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
+      if (!__s.__data[__i])
         return false;
     return true;
   }
 
   static bool __any_of(_Mask __s) noexcept {
-    for (auto __v : __s.__data)
-      if (__v)
+    for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
+      if (__s.__data[__i])
         return true;
     return false;
   }
 
   static bool __none_of(_Mask __s) noexcept {
-    for (auto __v : __s.__data)
-      if (__v)
+    for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
+      if (__s.__data[__i])
         return false;
     return true;
   }
 
   static bool __some_of(_Mask __s) noexcept {
     for (size_t __i = 1; __i < _Abi::__simd_size; ++__i)
-      if (__s[__i] != __s[__i - 1])
+      if (__s.__data[__i] != __s.__data[__i - 1])
         return true;
     return false;
   }
 
   static int __popcount(_Mask __s) noexcept {
     int __count = 0;
-    for (auto __v : __s.__data)
-      __count += __v != 0;
+    for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
+      __count += __s.__data[__i] != 0;
     return __count;
   }
 
   static int __find_first_set(_Mask __s) {
-    int __i = 0;
+    size_t __i = 0;
     for (; __i < _Abi::__simd_size; ++__i)
       if (__s.__data[__i])
         break;
@@ -288,8 +312,8 @@ struct __mask_traits {
   }
 
   static int __find_last_set(_Mask __s) {
-    int __i = _Abi::__simd_size - 1;
-    for (; __i != -1; --__i)
+    size_t __i = _Abi::__simd_size - 1;
+    for (; __i >= 0; --__i)
       if (__s.__data[__i])
         break;
     return __i;
@@ -306,47 +330,42 @@ struct __mask_traits {
       __s.__data[__i] = __m.__data[__i] ? __v : __s.__data[__i];
     return __s;
   }
-#define _LIBCXX_MASKED_OP_M(__op, __name)				\
-  static _Mask __masked##__name(_Mask&__s, _Mask __m, _Tp __v) noexcept \
-  {									\
-     for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)		\
-       __s.__data[__i] = __m.__data[__i] ? __s.__data[__i] __op __v :   \
-	  __s.__data[__i];						\
-     return __s;							\
-  }
-  _LIBCXX_MASKED_OP_M(+, _plus)
-  _LIBCXX_MASKED_OP_M(-, _minus)
-  _LIBCXX_MASKED_OP_M(*, _multiplues)
-  _LIBCXX_MASKED_OP_M(/, _divides)
-  _LIBCXX_MASKED_OP_M(%, _modulus)
-  _LIBCXX_MASKED_OP_M(&, _bit_and)
-  _LIBCXX_MASKED_OP_M(|, _bit_or)
-  _LIBCXX_MASKED_OP_M(^, _bit_xor)
-  _LIBCXX_MASKED_OP_M(<<, _shift_left)
-  _LIBCXX_MASKED_OP_M(>>, _shift_right)
-#undef _LIBCXX_MASKED_OP_M
-  static _Mask __masked_incre(_Mask& __s, _Mask __m) noexcept {
+  #define _LIBCXX_MASKED_OP_M(__op, __name)                               \
+    static void __masked##__name(_Mask&__s, _Mask __m, _Tp __v) noexcept  \
+    {                                                                     \
+      for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)                \
+        __s.__data[__i] = __m.__data[__i] ? __s.__data[__i] __op __v :    \
+      __s.__data[__i];                                                    \
+    }
+    _LIBCXX_MASKED_OP_M(+, _plus)
+    _LIBCXX_MASKED_OP_M(-, _minus)
+    _LIBCXX_MASKED_OP_M(*, _multiplues)
+    _LIBCXX_MASKED_OP_M(/, _divides)
+    _LIBCXX_MASKED_OP_M(%, _modulus)
+    _LIBCXX_MASKED_OP_M(&, _bit_and)
+    _LIBCXX_MASKED_OP_M(|, _bit_or)
+    _LIBCXX_MASKED_OP_M(^, _bit_xor)
+    _LIBCXX_MASKED_OP_M(<<, _shift_left)
+    _LIBCXX_MASKED_OP_M(>>, _shift_right)
+  #undef _LIBCXX_MASKED_OP_M
+  static void __masked_incre(_Mask& __s, _Mask __m) noexcept {
     for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
       __s.__data[__i] = __m.__data[__i] ? __s.__data[__i]++ : __s.__data[__i];
-    return __s;
   }
-  static _Mask __masked_decre(_Mask& __s, _Mask __m) noexcept {
+  static void __masked_decre(_Mask& __s, _Mask __m) noexcept {
     for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
       __s.__data[__i] = __m.__data[__i] ? __s.__data[__i]-- : __s.__data[__i];
-    return __s;
   }
   template<class _Up>
-  static _Mask __masked_load(_Mask& __s, _Mask __m, _Up* __mem) noexcept {
+  static void __masked_load(_Mask& __s, _Mask __m, _Up* __mem) noexcept {
     for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
-      __s.__data[__i] = __m.__data[__i] ? __mem[__i] : __s.__data[__i];
-    return __s;
+      __s.__data[__i] = __m.__data[__i] ?static_cast<_Tp>(__mem[__i]) : __s.__data[__i];
   }
  template<class _Up>
- static _Mask __masked_store(_Mask& __s, _Mask __m, _Up* __mem) noexcept {
+ static void __masked_store(const _Mask& __s, _Mask __m, _Up* __mem) noexcept {
     for (size_t __i = 0; __i < _Abi::__simd_size; ++__i)
-      __mem[__i] = __m.__data[__i] ? __s.__data[__i] : __mem[__i];
-    return __s;
-  } 
+      __mem[__i] = __m.__data[__i] ? static_cast<_Up>(__s.__data[__i]) : __mem[__i];
+  }
 };
 
 template <typename Tuple, typename Func, size_t ... N>
