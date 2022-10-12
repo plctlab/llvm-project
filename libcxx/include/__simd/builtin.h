402891412da8e5bb5165c209754f409b23f92821
Zhangyin <zhangyin2018@iscas.ac.cn>
2022-10-12 13:42:27 +0800

    [simd] Fixed licenses

--- libcxx/include/__simd/builtin.h
+++ libcxx/include/__simd/builtin.h
@@ -1,11 +1,12 @@
 // -*- C++ -*-
-//===-------------------------- __simd/abi/builtin.h -------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+
 #ifndef _LIBCPP___SIMD_BUILTIN_H
 #define _LIBCPP___SIMD_BUILTIN_H
 
