0629d364a67620eafb38338ead95a3a95ff212fd
fepicture <shyhot@outlook.com>
2022-10-21 06:25:55 +0000

    [simd] add reference_operators

--- libcxx/test/std/experimental/simd/simd.reference/reference_operators.pass.cpp
+++ libcxx/test/std/experimental/simd/simd.reference/reference_operators.pass.cpp
@@ -32,23 +32,324 @@
 #include <experimental/simd>
 
 namespace ex = std::experimental::parallelism_v2;
+
+template <class T>
+struct is_simd_reference : std::false_type {};
+
+template <class _Storage, class _ValueType>
+struct is_simd_reference<ex::__simd_reference<_Storage, _ValueType>> : std::true_type {};
+
 struct CheckReferenceOperators {
   template <class _Tp, class SimdAbi>
   void operator()() {
-    ex::simd<_Tp, SimdAbi> origin_simd([](_Tp i){return static_cast<_Tp>(i+2);});
+    ex::simd<_Tp, SimdAbi> origin_simd([](_Tp i) { return static_cast<_Tp>(i + 2); });
 
     {
       assert(origin_simd[0] == static_cast<_Tp>(2));
+
       using U = _Tp;
+      _Tp origin_val = static_cast<_Tp>(3);
       U x = static_cast<U>(3);
-      //if  (std::declval<_Tp&>() == std::forward<U>(x)) 
+
       {
-        origin_simd[0]= std::move(x);
+        decltype(std::declval<_Tp&>()) check_assgin(origin_val);
+        check_assgin = std::forward<U>(x);
       }
+
+      auto res = origin_simd[0].operator=(x);
       assert(origin_simd[0] == static_cast<_Tp>(3));
+      static_assert(is_simd_reference<decltype(res)>::value);
+      assert(res == static_cast<_Tp>(3));
     }
     {
+      assert(origin_simd[0] == static_cast<_Tp>(3));
 
+      using U = _Tp;
+      _Tp origin_val = static_cast<_Tp>(4);
+      U x = static_cast<U>(4);
+
+      {
+        decltype(std::declval<_Tp&>()) check_add_assgin(origin_val);
+        check_add_assgin += std::forward<U>(x);
+      }
+
+      auto res = origin_simd[0].operator+=(x);
+      assert(origin_simd[0] == static_cast<_Tp>(7));
+      static_assert(is_simd_reference<decltype(res)>::value);
+      assert(res == static_cast<_Tp>(7));
+    }
+    {
+      assert(origin_simd[0] == static_cast<_Tp>(7));
+
+      using U = _Tp;
+      _Tp origin_val = static_cast<_Tp>(6);
+      U x = static_cast<U>(6);
+
+      {
+        decltype(std::declval<_Tp&>()) check_minus_assgin(origin_val);
+        check_minus_assgin -= std::forward<U>(x);
+      }
+
+      auto res = origin_simd[0].operator-=(x);
+      assert(origin_simd[0] == static_cast<_Tp>(1));
+      static_assert(is_simd_reference<decltype(res)>::value);
+      assert(res == static_cast<_Tp>(1));
+    }
+    {
+      assert(origin_simd[0] == static_cast<_Tp>(1));
+
+      using U = _Tp;
+      _Tp origin_val = static_cast<_Tp>(12);
+      U x = static_cast<U>(12);
+
+      {
+        decltype(std::declval<_Tp&>()) check_mul_assgin(origin_val);
+        check_mul_assgin *= std::forward<U>(x);
+      }
+
+      auto res = origin_simd[0].operator*=(x);
+      assert(origin_simd[0] == static_cast<_Tp>(12));
+      static_assert(is_simd_reference<decltype(res)>::value);
+      assert(res == static_cast<_Tp>(12));
+    }
+    {
+      assert(origin_simd[0] == static_cast<_Tp>(12));
+
+      using U = _Tp;
+      _Tp origin_val = static_cast<_Tp>(2);
+      U x = static_cast<U>(2);
+
+      {
+        decltype(std::declval<_Tp&>()) check_div_assgin(origin_val);
+        check_div_assgin /= std::forward<U>(x);
+      }
+
+      auto res = origin_simd[0].operator/=(x);
+      assert(origin_simd[0] == static_cast<_Tp>(6));
+      static_assert(is_simd_reference<decltype(res)>::value);
+      assert(res == static_cast<_Tp>(6));
+    }
+    {
+      if constexpr (!std::is_floating_point_v<_Tp>) {
+        assert(origin_simd[0] == static_cast<_Tp>(6));
+
+        using U = _Tp;
+        _Tp origin_val = static_cast<_Tp>(4);
+        U x = static_cast<U>(4);
+
+        {
+          decltype(std::declval<_Tp&>()) check_mod_assgin(origin_val);
+          check_mod_assgin %= std::forward<U>(x);
+        }
+
+        auto res = origin_simd[0].operator%=(x);
+        assert(origin_simd[0] == static_cast<_Tp>(2));
+        static_assert(is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(2));
+      }
+    }
+    {
+      if constexpr (!std::is_floating_point_v<_Tp>) {
+        assert(origin_simd[0] == static_cast<_Tp>(2));
+
+        using U = _Tp;
+        _Tp origin_val = static_cast<_Tp>(5);
+        U x = static_cast<U>(5);
+
+        {
+          decltype(std::declval<_Tp&>()) check_or_assgin(origin_val);
+          check_or_assgin |= std::forward<U>(x);
+        }
+
+        auto res = origin_simd[0].operator|=(x);
+        assert(origin_simd[0] == static_cast<_Tp>(7));
+        static_assert(is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(7));
+      }
+    }
+    {
+      if constexpr (!std::is_floating_point_v<_Tp>) {
+        assert(origin_simd[0] == static_cast<_Tp>(7));
+
+        using U = _Tp;
+        _Tp origin_val = static_cast<_Tp>(3);
+        U x = static_cast<U>(3);
+
+        {
+          decltype(std::declval<_Tp&>()) check_and_assgin(origin_val);
+          check_and_assgin &= std::forward<U>(x);
+        }
+
+        auto res = origin_simd[0].operator&=(x);
+        assert(origin_simd[0] == static_cast<_Tp>(3));
+        static_assert(is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(3));
+      }
+    }
+    {
+      if constexpr (!std::is_floating_point_v<_Tp>) {
+        assert(origin_simd[0] == static_cast<_Tp>(3));
+
+        using U = _Tp;
+        _Tp origin_val = static_cast<_Tp>(8);
+        U x = static_cast<U>(8);
+
+        {
+          decltype(std::declval<_Tp&>()) check_exor_assgin(origin_val);
+          check_exor_assgin ^= std::forward<U>(x);
+        }
+
+        auto res = origin_simd[0].operator^=(x);
+        assert(origin_simd[0] == static_cast<_Tp>(11));
+        static_assert(is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(11));
+      }
+    }
+    {
+      if constexpr (!std::is_floating_point_v<_Tp>) {
+        assert(origin_simd[0] == static_cast<_Tp>(11));
+
+        using U = _Tp;
+        _Tp origin_val = static_cast<_Tp>(2);
+        U x = static_cast<U>(2);
+
+        {
+          decltype(std::declval<_Tp&>()) check_left_shift_assgin(origin_val);
+          check_left_shift_assgin <<= std::forward<U>(x);
+        }
+
+        auto res = origin_simd[0].operator<<=(x);
+        assert(origin_simd[0] == static_cast<_Tp>(44));
+        static_assert(is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(44));
+      }
+    }
+    {
+      if constexpr (!std::is_floating_point_v<_Tp>) {
+        assert(origin_simd[0] == static_cast<_Tp>(44));
+
+        using U = _Tp;
+        _Tp origin_val = static_cast<_Tp>(2);
+        U x = static_cast<U>(2);
+
+        {
+          decltype(std::declval<_Tp&>()) check_right_shift_assgin(origin_val);
+          check_right_shift_assgin >>= std::forward<U>(x);
+        }
+
+        auto res = origin_simd[0].operator>>=(x);
+        assert(origin_simd[0] == static_cast<_Tp>(11));
+        static_assert(is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(11));
+      }
+    }
+    {
+      if constexpr (!std::is_floating_point_v<_Tp>) {
+        assert(origin_simd[0] == static_cast<_Tp>(11));
+
+        _Tp origin_val = static_cast<_Tp>(2);
+
+        { origin_val++; }
+
+        auto res = origin_simd[0].operator++();
+        assert(origin_simd[0] == static_cast<_Tp>(12));
+        static_assert(is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(12));
+
+      } else {
+        assert(origin_simd[0] == static_cast<_Tp>(6));
+
+        _Tp origin_val = static_cast<_Tp>(2);
+
+        { origin_val++; }
+
+        auto res = origin_simd[0].operator++();
+        assert(origin_simd[0] == static_cast<_Tp>(7));
+        static_assert(is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(7));
+      }
+    }
+    {
+      if constexpr (!std::is_floating_point_v<_Tp>) {
+        assert(origin_simd[0] == static_cast<_Tp>(12));
+
+        _Tp origin_val = static_cast<_Tp>(2);
+
+        { origin_val--; }
+
+        auto res = origin_simd[0].operator--();
+        assert(origin_simd[0] == static_cast<_Tp>(11));
+        static_assert(is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(11));
+
+      } else {
+        assert(origin_simd[0] == static_cast<_Tp>(7));
+
+        _Tp origin_val = static_cast<_Tp>(2);
+
+        { origin_val--; }
+
+        auto res = origin_simd[0].operator--();
+        assert(origin_simd[0] == static_cast<_Tp>(6));
+        static_assert(is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(6));
+      }
+    }
+    {
+      if constexpr (!std::is_floating_point_v<_Tp>) {
+        assert(origin_simd[0] == static_cast<_Tp>(11));
+
+        _Tp origin_val = static_cast<_Tp>(2);
+
+        { origin_val++; }
+
+        auto res = origin_simd[0].operator++(origin_val);
+
+        assert(origin_simd[0] == static_cast<_Tp>(12));
+        static_assert(!is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(11));
+
+      } else {
+        assert(origin_simd[0] == static_cast<_Tp>(6));
+
+        _Tp origin_val = static_cast<_Tp>(2);
+
+        { origin_val++; }
+
+        auto res = origin_simd[0].operator++(origin_val);
+
+        assert(origin_simd[0] == static_cast<_Tp>(7));
+        static_assert(!is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(6));
+      }
+    }
+    {
+      if constexpr (!std::is_floating_point_v<_Tp>) {
+        assert(origin_simd[0] == static_cast<_Tp>(12));
+
+        _Tp origin_val = static_cast<_Tp>(2);
+
+        { origin_val--; }
+
+        auto res = origin_simd[0].operator--(origin_val);
+
+        assert(origin_simd[0] == static_cast<_Tp>(11));
+        static_assert(!is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(12));
+
+      } else {
+        assert(origin_simd[0] == static_cast<_Tp>(7));
+
+        _Tp origin_val = static_cast<_Tp>(2);
+
+        { origin_val--; }
+
+        auto res = origin_simd[0].operator--(origin_val);
+
+        assert(origin_simd[0] == static_cast<_Tp>(6));
+        static_assert(!is_simd_reference<decltype(res)>::value);
+        assert(res == static_cast<_Tp>(7));
+      }
     }
   }
 };
