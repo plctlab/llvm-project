5a6f594a986e2d0435920ec6ce13a7c51afa63b4
fepicture <shyhot@outlook.com>
2022-11-15 19:49:21 -0500

    [simd] update to_fixed_size

--- libcxx/test/std/experimental/simd/simd.casts/to_fixed_size.pass.cpp
+++ libcxx/test/std/experimental/simd/simd.casts/to_fixed_size.pass.cpp
@@ -24,18 +24,19 @@ namespace ex = std::experimental::parallelism_v2;
 struct CheckToFixedSizeSimd {
   template <class _Tp, class SimdAbi>
   void operator()() {
-    static_assert(std::is_same_v<decltype(ex::to_fixed_size<_Tp, SimdAbi>(ex::simd<_Tp, SimdAbi>())),
-                                 ex::fixed_size_simd<_Tp, origin.size()>>,
-                  "");
     static auto InitializeSimd = [](auto& origin_simd) {
       const size_t simd_size = origin_simd.size();
       for (size_t i = 0; i < simd_size; ++i)
         origin_simd[i] = static_cast<_Tp>(i);
     };
     {
-      auto origin = ex::simd<_Tp, SimdAbi>();
+      ex::simd<_Tp, SimdAbi> origin;
       InitializeSimd(origin);
       auto after_cast = ex::to_fixed_size<_Tp, SimdAbi>(origin);
+      static_assert(std::is_same_v<
+                        ex::fixed_size_simd<_Tp, ex::simd_size_v<_Tp, SimdAbi>>,
+                        decltype(after_cast)>,
+                    "");
       std::array<_Tp, origin.size()> expected_values;
       for (size_t i = 0; i < origin.size(); ++i)
         expected_values[i] = static_cast<_Tp>(i);
@@ -46,18 +47,20 @@ struct CheckToFixedSizeSimd {
 struct CheckToFixedSizeSimdMask {
   template <class _Tp, class SimdAbi>
   void operator()() {
-    static_assert(std::is_same_v<decltype(ex::to_fixed_size<_Tp, SimdAbi>(ex::simd<_Tp, SimdAbi>())),
-                                 ex::fixed_size_simd<_Tp, origin.size()>>,
-                  "");
     static auto InitializeSimdMask = [](auto& origin_simd_mask) {
       const size_t mask_size = origin_simd_mask.size();
       for (size_t i = 0; i < mask_size; ++i)
         origin_simd_mask[i] = static_cast<bool>(i);
     };
     {
-      auto origin = ex::simd_mask<_Tp, SimdAbi>();
+      ex::simd_mask<_Tp, SimdAbi> origin{};
       InitializeSimdMask(origin);
       auto after_cast = ex::to_fixed_size<_Tp, SimdAbi>(origin);
+      static_assert(
+          std::is_same_v<
+              ex::fixed_size_simd_mask<_Tp, ex::simd_size_v<_Tp, SimdAbi>>,
+              decltype(after_cast)>,
+          "");
       std::array<_Tp, origin.size()> expected_values;
       for (size_t i = 0; i < origin.size(); ++i)
         expected_values[i] = static_cast<bool>(i);
